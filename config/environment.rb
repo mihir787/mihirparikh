b284fdadf1d40335e2b91430e6ce59c1feabf77b
Mihir Parikh <mihir787@gmail.com>
2015-07-06 08:35:33 -0600

    updated

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
       user_name: ENV["SENDGRID_USERNAME"],
       password: ENV["SENDGRID_PASSWORD"],
-      domain: "http://mihirparikh.co",
+      domain: "https://mihirparikh.herokuapp.com/",
       address: "smtp.sendgrid.net",
       port: 587,
       authentication: :plain,
