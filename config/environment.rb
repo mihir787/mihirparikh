2d740ce4f69caaef4114ad6f42948bd059c5cd11
Mihir Parikh <mihir787@gmail.com>
2015-07-06 08:09:45 -0600

    changed nav

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
       user_name: ENV["SENDGRID_USERNAME"],
       password: ENV["SENDGRID_PASSWORD"],
-      domain: "http://localhost:3000",
+      domain: "http://mihirparikh.co",
       address: "smtp.sendgrid.net",
       port: 587,
       authentication: :plain,
